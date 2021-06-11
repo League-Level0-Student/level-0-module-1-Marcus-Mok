58dea033ebc18a77f47df39e4eeb5374405173bc
league <league@imac-25.attlocal.net>
2021-06-11 16:59:26 -0700

    Unbirthday, Vein Diagram

---
+++ src/_03_if_else/_1_unbirthday/Unbirthday.java
@@ -0,0 +1,22 @@
+package _03_if_else._1_unbirthday;
+
+import javax.swing.JOptionPane;
+
+public class Unbirthday {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String Birthday = JOptionPane.showInputDialog ("What is your birthday, (mm/dd)");
+		if  (Birthday.equalsIgnoreCase("06/11")); 
+		JOptionPane.showMessageDialog(null, "Happy Birthday");
+
+	
+	
+	
+	
+	
+	
+	}
+
+}
